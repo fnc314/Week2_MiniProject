ae6130c2a7a8be05fdc1703f74fe5b4d2530fff7
fnc314 <fnc314@gmail.com>
2014-01-20 13:42:30 -0800

    added new things

--- app.rb
+++ app.rb
@@ -3,6 +3,20 @@ require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
 require 'pry'
+require 'pg'
+
+# def create_movies_table
+#   c = PGconn.new(:host => "localhost", :dbname => "test")
+#   c.exec %q{
+#   CREATE TABLE movies (
+#     id SERIAL PRIMARY KEY,
+#     imdbID TEXT,
+#   );
+#   }
+#   c.close
+# end
+
+# create_movies_table
 
 get "/" do
 	erb :index	
@@ -24,12 +38,21 @@ end
 get '/details/:imdb' do |imdb_id|
   request = Typhoeus.get("www.omdbapi.com", :params => {:i => imdb_id, :plot => "full", :tomatoes => "true"})
   @id = JSON.parse(request.body)
-  puts @id
   if @id["Poster"] == "N/A"
   	@id["Poster"] = "http://1.bp.blogspot.com/-QVzzEVHIUxA/UDDNCSGQEqI/AAAAAAAAB0U/4iWJTRhH_AQ/s640/20-no-energy-robin-hegarty-ireland-thumb.jpg"
   else
   	@id
   end
-	puts @id
+  a = PGconn.new(:host => "localhost", :dbname => "FrancoNColaizzi")
+  a.exec_params("INSERT INTO movies (title, imdbID) VALUES('#{@id["Title"]}','#{@id["imdbID"]}');" )
+  a.close
   erb :details
+end
+
+get '/history' do
+  c = PGconn.new(:host => "localhost", :dbname => "FrancoNColaizzi")
+  hist = c.exec_params("SELECT * FROM movies;")
+  c.close
+  @hist = hist.to_a
+  erb :history
 end
